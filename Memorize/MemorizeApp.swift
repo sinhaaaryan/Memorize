96aaf7f6d790db8e98b44e89fb6c19fe511c3800
aaryansinha <sinha.aar@northeastern.edu>
2023-12-03 18:34:12 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Aaryan Sinha on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
